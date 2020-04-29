b58f82c6c92235b4234dfcf30d10990b01299f1d
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2020-04-29 17:44:58 -0300

    signin view

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,71 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logo from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logo} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('ok');
+              }}
+            >
+              Entrar
+            </Button>
+            <ForgotPassword
+              onPress={() => {
+                console.log('esqueci');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton
+        onPress={() => {
+          console.log('esqueci');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountText>Esqueci minha senha</CreateAccountText>
+      </CreateAccountButton>
+    </>
+  );
 };
 
 export default SignIn;
